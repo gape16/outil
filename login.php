6b329de540412519000e04a210bae44c24eb6cff
Henri LOEVENBRUCK <hloevenbruck@pjms.dom>
2017-09-21 16:04:15 +0200

    Changement du HTML

--- login.php
+++ login.php
@@ -37,6 +37,8 @@
 	<link rel="stylesheet" type="text/css" href="css/bootstrap-select.css">
 	<!-- <embed type="image/svg+xml" src="icons.svg" /> -->
 	
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="css/main.css">
 
 </head>
 
@@ -57,33 +59,6 @@
 						<img src="img/logo.png" alt="Olympus">
 						<h5 class="logo-title">olympus</h5>
 					</a>
-
-					<ul class="profile-menu">
-						<li>
-							<a href="#">About Us</a>
-						</li>
-						<li>
-							<a href="#">Careers</a>
-						</li>
-						<li>
-							<a href="#">FAQS</a>
-						</li>
-						<li>
-							<a href="#">Help & Support</a>
-						</li>
-						<li>
-							<a href="#" class="js-expanded-menu">
-								<svg class="olymp-menu-icon"><svg id="olymp-menu-icon" viewBox="0 0 41 32" width="100%" height="100%">
-									<title>menu-icon</title>
-									<path d="M4.571 0h-4.571v4.571h4.571v-4.571zM9.143 0v4.571h32v-4.571h-32zM13.714 13.714h-13.714v4.571h13.714v-4.571zM18.286 13.714v4.571h4.571v-4.571h-4.571zM27.429 18.286h13.714v-4.571h-13.714v4.571zM0 32h32v-4.569h-32v4.569zM36.571 32h4.571v-4.569h-4.571v4.569z"></path>
-								</svg></svg>
-								<svg class="olymp-close-icon"><svg id="olymp-close-icon" viewBox="0 0 32 32" width="100%" height="100%">
-									<title>close-icon</title>
-									<path d="M14.222 17.778h3.556v-3.556h-3.556v3.556zM31.084 3.429l-2.514-2.514-10.057 10.057 2.514 2.514 10.057-10.057zM0.916 28.571l2.514 2.514 10.057-10.055-2.516-2.514-10.055 10.055zM18.514 21.029l10.057 10.055 2.514-2.514-10.057-10.055-2.514 2.514zM0.916 3.431l10.057 10.055 2.516-2.514-10.059-10.057-2.514 2.516z"></path>
-								</svg></svg>
-							</a>
-						</li>
-					</ul>
 				</div>
 			</div>
 		</div>
@@ -97,11 +72,9 @@
 		<div class="row display-flex">
 			<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12">
 				<div class="landing-content">
-					<h1>Welcome to the Biggest Social Network in the World</h1>
-					<p>We are the best and biggest social network with 5 billion active users all around the world. Share you
-						thoughts, write blog posts, show your favourite music via Stopify, earn badges and much more!
+					<h1>Check ton site</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis alias molestiae fuga accusantium, expedita nam natus in dolores dignissimos, repellat placeat necessitatibus porro unde sunt amet quasi, consequatur itaque repudiandae.
 					</p>
-					<a href="#" class="btn btn-md btn-border c-white">Register Now!</a>
 				</div>
 			</div>
 
@@ -130,73 +103,61 @@
 					<!-- Tab panes -->
 					<div class="tab-content">
 						<div class="tab-pane active" id="home" role="tabpanel" data-mh="log-tab">
-							<div class="title h6">Register to Olympus</div>
+							<div class="title h6">Connectez-vous</div>
 							<form class="content">
 								<div class="row">
 									<div class="col-lg-6 col-md-6">
 										<div class="form-group label-floating is-empty">
-											<label class="control-label">First Name</label>
+											<label class="control-label">Prénom</label>
 											<input class="form-control" placeholder="" type="text">
 										</div>
 									</div>
 									<div class="col-lg-6 col-md-6">
 										<div class="form-group label-floating is-empty">
-											<label class="control-label">Last Name</label>
+											<label class="control-label">Nom</label>
 											<input class="form-control" placeholder="" type="text">
 										</div>
 									</div>
 									<div class="col-xl-12 col-lg-12 col-md-12">
 										<div class="form-group label-floating is-empty">
-											<label class="control-label">Your Email</label>
+											<label class="control-label">Ton email</label>
 											<input class="form-control" placeholder="" type="email">
 										</div>
 										<div class="form-group label-floating is-empty">
-											<label class="control-label">Your Password</label>
+											<label class="control-label">Ton mot de passe</label>
 											<input class="form-control" placeholder="" type="password">
 										</div>
 
 										<div class="form-group date-time-picker label-floating">
-											<label class="control-label">Your Birthday</label>
-											<input name="datetimepicker" value="10/24/1984" />
+											<label class="control-label">Ta date de naissance</label>
+											<input name="datetimepicker" value="10/11/1984" />
 											<span class="input-group-addon">
 												<svg class="olymp-calendar-icon"><use xlink:href="#olymp-calendar-icon"></use></svg>
 											</span>
 										</div>
 
-										<div class="form-group label-floating is-select">
-											<label class="control-label">Your Gender</label>
-											<select class="selectpicker form-control" size="auto">
-												<option value="MA">Male</option>
-												<option value="FE">Female</option>
-											</select>
-										</div>
+										<select class="selectpicker  form-control" size="auto">
+											<option value="MA">Graph</option>
+											<option value="FE">Chef d'équipe</option>
+										</select>
 
-										<div class="remember">
-											<div class="checkbox">
-												<label>
-													<input name="optionsCheckboxes" type="checkbox">
-													I accept the <a href="#">Terms and Conditions</a> of the website
-												</label>
-											</div>
-										</div>
-
-										<a href="#" class="btn btn-purple btn-lg full-width">Complete Registration!</a>
+										<a href="#" class="btn btn-purple btn-lg full-width">Termine ton inscription !</a>
 									</div>
 								</div>
 							</form>
 						</div>
 
 						<div class="tab-pane" id="profile" role="tabpanel" data-mh="log-tab">
-							<div class="title h6">Login to your Account</div>
+							<div class="title h6">Connecte toi à ton compte</div>
 							<form class="content">
 								<div class="row">
 									<div class="col-xl-12 col-lg-12 col-md-12">
 										<div class="form-group label-floating is-empty">
-											<label class="control-label">Your Email</label>
+											<label class="control-label">Ton Email</label>
 											<input class="form-control" placeholder="" type="email">
 										</div>
 										<div class="form-group label-floating is-empty">
-											<label class="control-label">Your Password</label>
+											<label class="control-label">Ton mot de passe</label>
 											<input class="form-control" placeholder="" type="password">
 										</div>
 
@@ -208,19 +169,10 @@
 													Remember Me
 												</label>
 											</div>
-											<a href="#" class="forgot">Forgot my Password</a>
+											<a href="#" class="forgot">Mot de passe oublié</a>
 										</div>
 
-										<a href="#" class="btn btn-lg btn-primary full-width">Login</a>
-
-										<div class="or"></div>
-
-										<a href="#" class="btn btn-lg bg-facebook full-width btn-icon-left"><i class="fa fa-facebook" aria-hidden="true"></i>Login with Facebook</a>
-
-										<a href="#" class="btn btn-lg bg-twitter full-width btn-icon-left"><i class="fa fa-twitter" aria-hidden="true"></i>Login with Twitter</a>
-
-
-										<p>Don’t you have an account? <a href="#">Register Now!</a> it’s really simple and you can start enjoing all the benefits!</p>
+										<a href="#" class="btn btn-lg btn-primary full-width">Connecte toi!</a>
 									</div>
 								</div>
 							</form>
