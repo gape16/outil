937e73d2dd864e627729e93ea48ecd8fa06ebf82
gaylord <gaylord.petit@gmail.com>
2017-09-25 12:25:21 +0200

    ajout de l'ajax pour test code jobuser

---
+++ formulaire.php
@@ -0,0 +1,11 @@
+<?php
+include('connexion_session.php');
+
+if (isset($_POST['jobUser'])) {
+	$job = $_POST['jobUser'];
+	$query_test_code = $bdd->prepare("SELECT code FROM statut WHERE id_statut = :code");
+	$query_test_code->bindParam(':code', $job);
+	$query_test_code->execute();
+	$test_code = $query_test_code->fetch();
+	echo $test_code["code"];
+})
