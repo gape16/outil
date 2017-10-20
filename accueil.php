7e42b514e0292ee521e8cc1f8dd329d7f58ff71d
gaylord <gaylord.petit@gmail.com>
2017-10-20 12:02:54 +0200

    accueil

--- accueil.php
+++ accueil.php
@@ -8,7 +8,7 @@ if (isset($_SESSION['id_statut'])) {
 	$id_graph=$_SESSION['id_graph'];
 	// si c'est un graph qui se connect
 	if ($_SESSION['id_statut'] == 1 || $_SESSION['id_statut'] == 2) {
-		$query_select_card_crea_maquette = $bdd->prepare("SELECT num_client, raison_social, lien_CMS, photo, IDGPP FROM client inner join user on client.id_graph_maquette=user.id_user where client.id_graph_maquette=? and (id_etat = 1 or id_etat= 3 or id_etat = 4 or id_etat = 6)");
+		$query_select_card_crea_maquette = $bdd->prepare("SELECT num_client, raison_social, lien_CMS, photo, IDGPP, id_etat FROM client inner join user on client.id_graph_maquette=user.id_user where client.id_graph_maquette=? and (id_etat = 1 or id_etat= 3 or id_etat = 4 or id_etat = 6)");
 		$query_select_card_crea_maquette->bindParam(1, $id_graph);
 		$query_select_card_crea_maquette->execute();
 		$cards_client=$query_select_card_crea_maquette->fetchAll();
@@ -133,7 +133,7 @@ if (isset($_SESSION['id_statut'])) {
 		<div class="container cards">
 			<div class="row">
 				<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-6">
-					<div class="friend-item friend-groups create-group" data-mh="friend-groups-item">
+					<div class="friend-item friend-groups create-group hauteur-card ajout-client" data-mh="friend-groups-item">
 						<a href="#" class="full-block" data-toggle="modal" data-target="#create-friend-group-1"></a>
 						<div class="content">
 							<a href="#" class="  btn btn-control bg-blue" data-toggle="modal" data-target="#create-friend-group-1">
@@ -149,33 +149,55 @@ if (isset($_SESSION['id_statut'])) {
 
 
 				<?php foreach ($cards_client as $key => $value) {?>
-
+				<?php
+				if($value['id_etat']==1){
+					$class_etat="crea-maquette";
+					$class_img="img/Crea-maquette.png";
+				}elseif ($value['id_etat']==2) {
+					$class_etat="ctrl-maquette";
+					$class_img="img/Ctrl-maquette.png";
+				}elseif ($value['id_etat']==3) {
+					$class_etat="retour-crea";
+					$class_img="img/Retour-crea.png";
+				}elseif ($value['id_etat']==4) {
+					$class_etat="crea-graphique";
+					$class_img="img/Crea-graphique.png";
+				}elseif ($value['id_etat']==5) {
+					$class_etat="ctrl-design";
+					$class_img="img/Ctrl-design.png";
+				}elseif ($value['id_etat']==6) {
+					$class_etat="retour-crea";
+					$class_img="img/Retour-graphique.png";
+				}elseif ($value['id_etat']==7) {
+					$class_etat="site-valide";
+					$class_img="img/Site-valide.png";
+				}
+				?>
 				<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-6">
-					<div class="ui-block box-diagonal" data-mh="friend-groups-item">
-						<div class="friend-item friend-groups card-accueil">
+					<div class="ui-block hauteur-card" data-mh="friend-groups-item">
+						<div class="friend-item friend-groups <?php echo $class_etat;?>">
 							<div class="friend-item-content">
-								<div class="friend-avatar">
-									<div class="author-thumb rond-card">
-										<img src="img/crea_maquette.png" alt="Olympus">
+								<div class="friend-avatar entete-card">
+									<div class="author-thumb etat-card">
+										<img src="<?php echo $class_img;?>" alt="Olympus">
 									</div>
-									<div class="author-content">
+									<div class="author-content texte-card">
 										<a href="#" class="h5 author-name"><?php echo utf8_encode($value['raison_social']);?></a>
 										<div class="country"><?php echo $value['num_client'];?></div>
 									</div>
 								</div>
-								<ul class="friends-harmonic">
-									<li>
-										<a href="#">
-											<img src="<?php echo utf8_encode($value['photo']);?>" alt="friend">
-										</a>
-									</li>
-								</ul>
-								<div class="control-block-button">
-									<a href="<?php echo utf8_encode($value['lien_CMS']);?>" target="_blank" class="  btn btn-control bg-blue" data-toggle="modal" data-target="#create-friend-group-add-friends">
-										<svg class="olymp-happy-faces-icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="icons/icons.svg#olymp-happy-faces-icon"></use></svg>
+								<div class="control-block-button bouton-check">
+									<a href="<?php echo utf8_encode($value['lien_CMS']);?>" target="_blank" class="  btn btn-control bg-blue bouton-icone1" data-toggle="modal" data-target="#create-friend-group-add-friends">
+										<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="35%" version="1.1" height="35%" viewBox="0 0 64 64" enable-background="new 0 0 64 64">
+											<path d="m60.135,3.875c-5.156-5.166-13.545-5.168-18.697,0l-11.576,11.619c-0.788,0.791-0.788,2.074 0,2.865 0.79,0.792 2.067,0.792 2.856,0l11.576-11.618c3.578-3.589 9.401-3.587 12.984,0 3.578,3.591 3.578,9.435 0,13.024l-15.292,15.339c-1.732,1.739-4.038,2.697-6.49,2.697-2.451,0-4.758-0.959-6.492-2.697-0.789-0.792-2.067-0.792-2.857,0-0.788,0.791-0.788,2.074 0,2.865 2.499,2.505 5.818,3.885 9.35,3.885s6.848-1.381 9.347-3.885l15.292-15.338c5.152-5.17 5.152-13.584-0.001-18.756z" fill="#FFFFFF"/>
+											<path d="m31.015,45.904l-11.312,11.346c-1.732,1.739-4.039,2.697-6.491,2.697-2.451,0-4.759-0.958-6.489-2.697-3.578-3.591-3.578-9.434 0-13.023l15.289-15.338c3.582-3.588 9.406-3.588 12.983,0 0.789,0.793 2.067,0.793 2.856,0 0.789-0.791 0.789-2.072 0-2.864-5.152-5.17-13.541-5.17-18.697,0l-15.288,15.336c-5.155,5.17-5.155,13.584 4.44089e-16,18.754 2.497,2.506 5.816,3.885 9.346,3.885 3.531,0 6.853-1.379 9.348-3.885l11.31-11.345c0.79-0.791 0.79-2.074 0-2.865-0.788-0.792-2.067-0.792-2.855-0.001z" fill="#FFFFFF"/>
+										</svg>
 									</a>
-									<a href="check.php?idgpp=<?php echo $value['IDGPP'];?>" class="btn btn-control btn-grey-lighter">
-										<svg class="olymp-settings-icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="icons/icons.svg#olymp-settings-icon"></use></svg>
+									<a href="check.php?idgpp=<?php echo $value['IDGPP'];?>" class="btn btn-control btn-grey-lighter bouton-icone2">
+										<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="35%" height="35%" viewBox="0 0 394.893 394.893" style="enable-background:new 0 0 394.893 394.893;" xml:space="preserve">
+											<path d="M344.426,191.963c-6.904,0-12.5,5.597-12.5,12.5V350.91H25V43.982h246.57c6.904,0,12.5-5.597,12.5-12.5    c0-6.903-5.596-12.5-12.5-12.5H12.5c-6.903,0-12.5,5.597-12.5,12.5V363.41c0,6.903,5.597,12.5,12.5,12.5h331.926    c6.902,0,12.5-5.597,12.5-12.5V204.463C356.926,197.56,351.33,191.963,344.426,191.963z" fill="#FFFFFF"/>
+											<path d="M391.23,27.204c-4.881-4.881-12.795-4.881-17.678,0L169.957,230.801l-50.584-50.584c-4.882-4.881-12.796-4.881-17.678,0    c-4.881,4.882-4.881,12.796,0,17.678l59.423,59.423c2.441,2.44,5.64,3.661,8.839,3.661c3.199,0,6.398-1.221,8.839-3.661    L391.23,44.882C396.113,40,396.113,32.086,391.23,27.204z" fill="#FFFFFF"/>
+										</svg>
 									</a>
 								</div>
 							</div>
